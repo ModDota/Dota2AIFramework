d99437b227de0634668ed8a1328b95d186c3a860
Perry <perry.wesel@gmail.com>
2015-10-04 18:19:00 +0200

    UnitWrapper API list completely implemented

--- Game/scripts/vscripts/AI/UnitWrapper.lua
+++ Game/scripts/vscripts/AI/UnitWrapper.lua
@@ -16,6 +16,9 @@ end
 
 --Wrap a unit for a set team
 function WrapUnit( unit, team )
+	--If the unit is nil just return
+	if unit == nil then return nil end
+
 	--Check if we wrapped already
 	if unit.wrapped ~= nil then
 		return unit.wrapped
@@ -40,6 +43,17 @@ end
 function UnitSetup( unit, globalUnit, team )
 
 	--[[
+		unit:IsInVision()
+		Return if the unit is in vision or not
+
+		Modification:Does not exist in regular API.
+		Parameters: -
+	]]
+	function unit:IsInVision()
+		return InVision( globalUnit, team )
+	end
+
+	--[[
 		unit:GetAbsOrigin()
 		Get the position of the unit.
 
@@ -47,7 +61,7 @@ function UnitSetup( unit, globalUnit, team )
 		Parameters: -
 	]]
 	function unit:GetAbsOrigin()
-		if InVision( unit, team ) then
+		if InVision( globalUnit, team ) then
 			return globalUnit:GetAbsOrigin()
 		else
 			return nil
@@ -55,14 +69,18 @@ function UnitSetup( unit, globalUnit, team )
 	end
 
 	--[[
-		unit:GetAbilityByIndex( index )
-		Retrieve an ability by index from the unit.
-		
-		Modification: -
+		unit:GetOrigin()
+		Get the position of the unit.
+
+		Modification: Return nil when unit is in fog of war.
 		Parameters: -
 	]]
-	function unit:GetAbilityByIndex( index )
-		return WrapAbility( globalUnit:GetAbilityByIndex( index ), team )
+	function unit:GetOrigin()
+		if InVision( globalUnit, team ) then
+			return globalUnit:GetOrigin()
+		else
+			return nil
+		end
 	end
 
 	--[[
@@ -74,5 +92,188 @@ function UnitSetup( unit, globalUnit, team )
 	]]
 	function unit:GetPlayerOwnerID()
 		return globalUnit:GetPlayerOwnerID()
+	end
+
+	--[[
+		unit:GetHealth()
+		Get the health of the unit.
+
+		Modification: Return nil when unit is in fog of war.
+		Parameters: -
+	]]
+	function unit:GetHealth()
+		if InVision( globalUnit, team ) then
+			return globalUnit:GetHealth()
+		else
+			return nil
+		end
+	end
+
+	--[[
+		unit:GetMaxHealth()
+		Get the maximum health of the unit.
+
+		Modification: Return nil when unit is in fog of war.
+		Parameters: -
+	]]
+	function unit:GetMaxHealth()
+		if InVision( globalUnit, team ) then
+			return globalUnit:GetMaxHealth()
+		else
+			return nil
+		end
+	end
+
+	--[[
+		unit:GetModelName()
+		Get the model name of the unit.
+
+		Modification: Return nil when unit is in fog of war.
+		Parameters: -
+	]]
+	function unit:GetModelName()
+		if InVision( globalUnit, team ) then
+			return globalUnit:GetModelName()
+		else
+			return nil
+		end
+	end
+
+	--[[
+		unit:GetOwner()
+		Get the owner of the unit.
+
+		Modification: Wrap result.
+		Parameters: -
+	]]
+	function unit:GetOwner()
+		return WrapUnit( globalUnit:GetOwner() )
+	end
+
+	--[[
+		unit:GetOwnerEntity()
+		Get the owner of the unit.
+
+		Modification: Wrap result.
+		Parameters: -
+	]]
+	function unit:GetOwnerEntity()
+		return WrapUnit( globalUnit:GetOwnerEntity() )
+	end
+
+	--[[
+		unit:GetTeam()
+		Get the team of the unit.
+
+		Modification: -
+		Parameters: -
+	]]
+	function unit:GetTeam()
+		return globalUnit:GetTeam()
+	end
+
+	--[[
+		unit:GetTeamNumber()
+		Get the team of the unit.
+
+		Modification: -
+		Parameters: -
+	]]
+	function unit:GetTeamNumber()
+		return globalUnit:GetTeamNumber()
+	end
+
+	--[[
+		unit:IsAlive()
+		Return if the unit is alive or not.
+
+		Modification: -
+		Parameters: -
+	]]
+	function unit:IsAlive()
+		if InVision( globalUnit, team ) then
+			return globalUnit:IsAlive()
+		else
+			return false
+		end
+	end
+
+	--[[
+		unit:GetAbilityByIndex( index )
+		Retrieve an ability by index from the unit.
+		
+		Modification: Wrap the result, only works if unit is in vision, nil otherwise.
+		Parameters:
+			* index - The index of the ability on the unit
+	]]
+	function unit:GetAbilityByIndex( index )
+		if InVision( globalUnit, team ) then
+			return WrapAbility( globalUnit:GetAbilityByIndex( index ), team )
+		else
+			return nil
+		end
+	end
+
+	--[[
+		unit:FindAbilityByName( name )
+		Retrieve an ability by index from the unit.
+		
+		Modification: -
+		Parameters:
+			* name - The name of the ability to look up.
+	]]
+	function unit:FindAbilityByName( name )
+		if InVision( globalUnit, team ) then
+			return WrapAbility( globalUnit:FindAbilityByName( name ), team )
+		else
+			return nil
+		end
+	end
+
+	--Entity functions
+	--==========================================================================
+
+	--[[
+		entity:GetClassname()
+		Get the classname of the entity.
+
+		Modification: -
+		Parameters: -
+	]]
+	function unit:GetClassname()
+		return globalUnit:GetClassname()
+	end
+
+	--[[
+		entity:GetEntityHandle()
+		Get the entity handle of the entity.
+
+		Modification: -
+		Parameters: -
+	]]
+	function unit:GetEntityHandle()
+		return globalUnit:GetEntityHandle()
+	end
+
+	--[[
+		entity:GetEntityIndex()
+		Get the entity index of the entity.
+
+		Modification: -
+		Parameters: -
+	]]
+	function unit:GetEntityIndex()
+		return globalUnit:GetEntityIndex()
+	end
+
+	--[[
+		entity:GetName()
+		Get the name of the entity.
+
+		Modification: -
+		Parameters: -
+	]]
+	function unit:GetName()
+		return globalUnit:GetName()
 	end
 end
