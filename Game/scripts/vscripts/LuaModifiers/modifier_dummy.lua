102634a737c9bcded393057ce9566655f5055d4e
Perry <perry.wesel@gmail.com>
2015-10-17 15:27:08 +0200

    Prevented vision dummies from being attackable/killable FailFish

--- Game/scripts/vscripts/LuaModifiers/modifier_dummy.lua
+++ Game/scripts/vscripts/LuaModifiers/modifier_dummy.lua
@@ -3,7 +3,10 @@ modifier_dummy = class({})
 --Set the dummy out of the game
 function modifier_dummy:CheckState()
 	local state = {
-		[MODIFIER_STATE_OUT_OF_GAME] = true
+		[MODIFIER_STATE_OUT_OF_GAME] = true,
+		[MODIFIER_STATE_INVULNERABLE] = true,
+		[MODIFIER_STATE_NO_UNIT_COLLISION] = true,
+		[MODIFIER_STATE_UNSELECTABLE] = true
 	}
 
 	return state
